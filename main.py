a55a7b79de66d9bbcab8eacf7b4eb68d20139658
Caeden Kidd <caedenmkidd@gmail.com>
2024-08-11 23:01:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # Please modify to meet your application
-PRINTER_MAC_ADDRESS = "YOUR MAC ADDRESS"
 WEB_ADDRESS = "YOUR_WEBSITE.COM/YOUR_PROJECT_PATH/picture_generator.php?auth_token=YOUR_AUTH_TOKEN"
-NAME_OF_PRINTER = "MX06"
 
 # Original import statements
 print("Application Opened. Loading imports...")
@@ -52,66 +50,12 @@ def blink_led_thread():
 led_thread = threading.Thread(target=blink_led_thread)
 led_thread.start()
 
-def bluetooth_power_restart():
-    try:
-        print("Restarting bluetooth...")
-        subprocess.run(['sudo', 'systemctl', 'restart', 'bluetooth'], check=True)
-        print("Bluetooth restarted.")
-    except subprocess.CalledProcessError as e:
-        set_led_pattern(".-..   ", "error")
-        raise SystemExit(f"Error restarting bluetooth: {e}")
-
-bluetooth_power_restart()
-set_led_pattern("-", "loading")
-time.sleep(5)
-set_led_pattern(".", "loading")
-
-def is_device_connected():
-    try:
-        # Execute hcitool command to get connected devices
-        result = subprocess.run(['hcitool', 'con'], stdout=subprocess.PIPE, text=True)
-        connections = result.stdout.splitlines()
-        
-        # Check each line for the MAC address
-        for line in connections:
-            if PRINTER_MAC_ADDRESS in line:
-                return True
-        return False
-    except subprocess.CalledProcessError as e:
-        set_led_pattern(".---.   ", "error")
-        raise SystemExit(f"Error checking Bluetooth connection: {e}")
-        return False
-
-def bluetooth_monitor():
-    while status != "error":
-        is_device_connected()
-
-        time.sleep(5)
-
+from Adafruit_Thermal import *
 from PIL import Image
-from printer import PrinterDriver
 import io
 import requests
 
-print("Loaded imports. Connecting to printer...")
-
-# Initialize PrinterDriver
-printer = PrinterDriver()
-
-printer.scan_time = 4
-printer.energy = 0xffff
-printer.speed = 180
-printer.quality = 40
-
-try:
-    printer.connect(name=NAME_OF_PRINTER, address=PRINTER_MAC_ADDRESS)
-except:
-    set_led_pattern("-.-", "error")
-    raise SystemExit("Cannot actively connect to bluetooth")
-
-
-print("Connected to printer.")
-
+printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 set_led_pattern(" ", "connected")
 
 finished_printing = True
@@ -122,9 +66,6 @@ def button_callback():
         print("Button pressed!")
         set_led_pattern(".", "loading")  # Indicate loading
         capture_and_print_screenshot()
-
-monitor_thread = threading.Thread(target=bluetooth_monitor)
-monitor_thread.start()
 
 def capture_and_print_screenshot():
     global finished_printing, WEB_ADDRESS
@@ -137,12 +78,7 @@ def capture_and_print_screenshot():
                 # Access the content of the response (image data in bytes)
                 image_data = io.BytesIO(response.content)
                 img = Image.open(image_data)
-                pbm_image = img.convert(mode='1')
-                pbm_bytes = io.BytesIO()
-                pbm_image.save(pbm_bytes, format='PPM')
-                pbm_bytes.seek(0)
-
-                printer.print(pbm_bytes, mode="default")
+                printer.printImage(img, True) # This does the printing
         else:
                 set_led_pattern(".-.   ", "semi_error")
                 print('Failed to download image. Status code:', response.status_code)
